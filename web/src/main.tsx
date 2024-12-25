b5d48310b0ca1b59ceef1c34c263743ab556e0b3
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-25 19:24:25 +0100

    add .env.examples

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -35,12 +35,6 @@ declare module "@tanstack/react-router" {
 	}
 }
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-	throw new Error("Missing Publishable Key")
-}
-
 const InnerApp = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
