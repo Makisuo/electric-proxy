0bbbf94dc8477f483460c9d834a4952ff34ec8b5
Makisuo <minetoaster12@gmail.com>
2025-04-04 00:17:15 +0200

    fix

--- backend/src/services/jose.ts
+++ backend/src/services/jose.ts
@@ -48,6 +48,7 @@ export class Jose extends Effect.Service<Jose>()("Jose", {
 					if (e instanceof errors.JWTExpired) {
 						return new JoseError({ message: "JWT Expired" })
 					}
+					console.log("JOSE_ERROR", e)
 					return new JoseError({ message: "Invalid JWT needs to have a sub" })
 				},
 			})
